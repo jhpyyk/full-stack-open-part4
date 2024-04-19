e558051b29ce47b4ed63c157b654133fdfe2989f
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2024-04-19 11:41:29 +0300

    Add favoriteBlog and unit test - exercise 4.5

--- tests/list_helper.test.ts
+++ tests/list_helper.test.ts
@@ -1,4 +1,4 @@
-import { test } from "node:test";
+import { describe, test } from "node:test";
 import assert from "node:assert";
 import listHelper from "../utils/list_helper";
 import { BlogType } from "../types";
@@ -8,4 +8,73 @@ test("dummy returns one", () => {
 
     const result = listHelper.dummy(blogs);
     assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+    test("of an empty list is 0", () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0);
+    });
+    test("of a list of length 1", () => {
+        const blog: BlogType = {
+            title: "a",
+            author: "a",
+            url: "a",
+            likes: 10,
+        };
+        assert.strictEqual(listHelper.totalLikes([blog]), blog.likes);
+    });
+    test("of a list of length 3", () => {
+        const blog1: BlogType = {
+            title: "a",
+            author: "a",
+            url: "a",
+            likes: 1,
+        };
+        const blog2: BlogType = {
+            title: "a",
+            author: "a",
+            url: "a",
+            likes: 2,
+        };
+        const blog3: BlogType = {
+            title: "a",
+            author: "a",
+            url: "a",
+            likes: 3,
+        };
+        assert.strictEqual(listHelper.totalLikes([blog1, blog2, blog3]), 6);
+    });
+});
+
+describe("favorite blog based on likes", () => {
+    const blog1: BlogType = {
+        title: "a",
+        author: "a",
+        url: "a",
+        likes: 1,
+    };
+    const blog2: BlogType = {
+        title: "b",
+        author: "b",
+        url: "b",
+        likes: 2,
+    };
+    const blog3: BlogType = {
+        title: "c",
+        author: "c",
+        url: "c",
+        likes: 3,
+    };
+    test("of an empty list is undefined", () => {
+        assert.strictEqual(listHelper.favoriteBlog([]), undefined);
+    });
+    test("of a list of length 1", () => {
+        assert.strictEqual(listHelper.favoriteBlog([blog1]), blog1);
+    });
+    test("of a list of length 3", () => {
+        assert.strictEqual(
+            listHelper.favoriteBlog([blog1, blog3, blog2]),
+            blog3
+        );
+    });
 });
--- utils/list_helper.ts
+++ utils/list_helper.ts
@@ -4,4 +4,29 @@ const dummy = (_blogs: BlogType[]) => {
     return 1;
 };
 
-export default { dummy };
+const totalLikes = (blogs: BlogType[]): number => {
+    const sumOfLikes = (sum: number, blog: BlogType) => {
+        return sum + blog.likes.valueOf();
+    };
+    const total = blogs.reduce(sumOfLikes, 0);
+    return total;
+};
+
+const favoriteBlog = (blogs: BlogType[]): BlogType | undefined => {
+    if (blogs.length === 0) {
+        return undefined;
+    }
+    const mostLikedBlog = (mostLiked: BlogType, blog: BlogType): BlogType => {
+        if (blog.likes.valueOf() > mostLiked.likes.valueOf()) {
+            return blog;
+        }
+        return mostLiked;
+    };
+    return blogs.reduce(mostLikedBlog, blogs[0]);
+};
+
+export default {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+};
